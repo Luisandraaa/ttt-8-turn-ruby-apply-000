26611c3631663c970e4bc2a1c736fbbb2d239025
luisandra abreu duran <abreuluisandra@gmail.com>
2020-06-09 05:12:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,7 +44,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  input_to_index(input)
+  index = input_to_index(input)
   if valid_move(board,index) == true
     move(array,input)
     display_board(board)
