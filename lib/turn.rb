9430b4f0b7d5149e1777b0d9b3f7dd3fa22e1a8f
luisandra abreu duran <abreuluisandra@gmail.com>
2020-06-09 05:19:19 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,17 @@
 
-def display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-   puts "-----------"
-   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-   puts "-----------"
-   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
 end
 
 def valid_move?(board, index)
@@ -32,21 +38,19 @@ def valid_move?(board, index)
   end
 end
 
-def input_to_index(input)
-  index = input.to_i - 1
-  return index
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
 
-def move(array,input,current_player ="X")
-  array[input] = current_player
-end
 
-def turn(board)
+def turn (board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move(board,index) == true
-    move(array,input)
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
     display_board(board)
   else
     turn(board)
